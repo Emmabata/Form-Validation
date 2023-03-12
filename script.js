8f92744a97de09435ede35aec6322cc3dedffac1
Emmabata <ndinezeemmanuel@gmail.com>
2023-03-12 04:42:18 +0100

    form-validation

---
+++ script.js
@@ -0,0 +1,51 @@
+ const myNam = document.getElementById("Name");
+ const email = document.getElementById("Email");
+ const phoneNumber = document.getElementById("phoneNum");
+ const phoneRegEx = new RegExp(/^[0-9]{11}$/);
+ const passWord = document.getElementById("pwrd");
+ const pattern = Element.getAttribute("pattern")
+
+
+//  FOR NAME
+ myNam.addEventListener("change", () => {
+    if(myNam.value.includes("@")) {
+        alert("symbols not needed in this section")
+    }
+    if(myNam.value.length < 3) {
+        alert("Name is less than three characters");
+    }
+ })
+
+
+//  FOR EMAIL
+email.addEventListener("change", () => {
+    if(!email.value.includes("@")) {
+        alert("Invalid email")
+    }
+})
+
+// FOR PHONE NUMBER
+phoneNumber.addEventListener("change", () => {
+     
+    if(!phoneNumber.value.includes([+234])) {
+        alert("input your country code")
+    }else{return true};
+
+    if(phoneNumber.value.length != 10) {
+        alert("invalid phone number")
+    }
+    if(!phoneNumber.test(phoneNumber.value)) {
+        alert("Phone number must contain only numbers")
+    }
+    
+})
+
+// FOR PASSWORD
+passWord.addEventListener("change", () => {
+    if(passWord.value.length < 5) {
+        alert("minimum of 5 characters needed")
+    }
+    if(passWord.value.length > 8) {
+        alert("maximum of 8 characters needed")
+    }
+})
